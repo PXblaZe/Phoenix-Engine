935f703d58ebcd9fd0a28f35af6b66d5d4261978
PXblaZe <gsumit178@gmail.com>
2023-06-11 19:01:18 +0530

    added vertex buffer layout.

---
+++ source/pe_vertexbufferlayout.cpp
@@ -0,0 +1,12 @@
+
+#include "PXE/pe_vertexbufferlayout.hpp"
+
+// #define
+
+namespace px {
+    
+    unsigned int VertexBufferLayout::VertexArrayAttrib(unsigned int GLtype) {
+        return 0;
+    }
+
+} // namespace px 
