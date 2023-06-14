2edce4ff5cdd13c8104cf8d799d9bd2d4e88f86e
PXblaZe <gsumit178@gmail.com>
2023-06-14 22:48:15 +0530

    some tweaks.

--- main.cpp
+++ main.cpp
@@ -6,18 +6,14 @@
 #include "PXE/pe_vertexbuffer.hpp"
 
 void rcb(int a , float b, const char* str) {
-
+    
 }
 
 int main() {
 
     // px::Window win(1, 1, "Phoenix Engine");
-    px::Window win("PX Engine");
-    // win.showWindow();
-    
-    px::VertexBuffer vb(4*50);
-    void* p = malloc(4*10);
-    vb.updateBufferData(p);
+    px::Window win("While Noise");
+
     win.run<int, float, const char*>(rcb, 10, 3.14, "Vipul");
 
     return 0;
