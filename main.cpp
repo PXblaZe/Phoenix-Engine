b855120b2228f1f239b1b0176d9f67532e280d5b
PXblaZe <gsumit178@gmail.com>
2023-07-08 12:21:03 +0530

    updated example code.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #define PX_DEBUG
 
 #include "PXE/pe_window.hpp"
+#include "PXE/pe_program.hpp"
 #include "PXE/pe_vertexbuffer.hpp"
 
 void rcb(int a , float b, const char* str) {
@@ -11,9 +12,19 @@ void rcb(int a , float b, const char* str) {
 
 int main() {
 
-    // px::Window win(1, 1, "Phoenix Engine");
-    px::Window win("While Noise");
+    px::Window win(800, 600, "Phoenix Engine");
+    // px::Window win("While Noise");
 
+    px::ShaderCode sc("/home/blaze/Codes/Cpp/GLEX/shaders/noiseShader.glsl");
+    px::ShaderProgram sp;
+    sp.createProgram(sc.parseCode("ver"), sc.parseCode("frag"));
+    px::ShaderProgram::use(sp);
+    // std::cout << sc.parseCode("ver") << std::endl << sc.parseCode("frag") << std::endl;
+    std::cout << "vsID: " << sp.getShaderID(px::ShaderProgram::VERTEX_SHADER) << std::endl;
+    std::cout << "fsID: " << sp.getShaderID(px::ShaderProgram::FRAGMENTSHADER) << std::endl;
+    sp.cleanShaders(px::ShaderProgram::FRAGMENTSHADER | px::ShaderProgram::VERTEX_SHADER);
+    std::cout << "vsID: " << sp.getShaderID(px::ShaderProgram::VERTEX_SHADER) << std::endl;
+    std::cout << "fsID: " << sp.getShaderID(px::ShaderProgram::FRAGMENTSHADER) << std::endl;
     win.run<int, float, const char*>(rcb, 10, 3.14, "Vipul");
 
     return 0;
