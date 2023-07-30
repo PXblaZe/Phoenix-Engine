06f0ccbfdd77ef795af1291d64125087096258fa
PXblaZe <gsumit178@gmail.com>
2023-07-31 04:17:46 +0530

    Implemented VertexBufferLayout class.

--- include/PXE/pe_vertexbufferlayout.hpp
+++ include/PXE/pe_vertexbufferlayout.hpp
@@ -8,21 +8,50 @@ class VertexBufferLayout {
     struct VertexArrayAttrib {
         unsigned int type;
         unsigned int count;
+        unsigned int type_size;
         unsigned char normalized;
-        static unsigned int getStride(unsigned int GLtype); 
-    };
 
+    };
+    unsigned int stride;
     std::vector<struct VertexBufferLayout::VertexArrayAttrib> layout;
 
-public:
-    template<typename T>
-    void attachAttrib(bool normalized = false)
+    inline void insert(unsigned int type, unsigned int count, unsigned char type_size, unsigned char normalized)
     {
-        layout.push_back({
-            GLtype<T>::enumval, 
-            layout.size(), 
-            normalized ? 1 : 0
-        });
+        layout.push_back({type, count, type_size, normalized});
+    }
+
+    void pushf(unsigned int count, bool normalized);
+    void pushd(unsigned int count, bool normalized);
+    void pushb(unsigned int count, bool normalized);
+    void pushub(unsigned int count, bool normalized);
+    void pushi(unsigned int count, bool normalized);
+    void pushui(unsigned int count, bool normalized);
+    void pushs(unsigned int count, bool normalized);
+    void pushus(unsigned int count, bool normalized);
+
+public:
+
+    VertexBufferLayout();
+
+    template<typename _Tp>
+    inline constexpr void push(unsigned int count, bool normalized = false) 
+    {
+        if constexpr (std::is_same<_Tp, float>::value) pushf(count, normalized);
+        else if constexpr (std::is_same<_Tp, double>::value) pushd(count, normalized);
+        else if constexpr (std::is_same<_Tp, signed char>::value) pushb(count, normalized);
+        else if constexpr (std::is_same<_Tp, unsigned char>::value) pushub(count, normalized);
+        else if constexpr (std::is_same<_Tp, signed int>::value) pushi(count, normalized);
+        else if constexpr (std::is_same<_Tp, unsigned int>::value) pushui(count, normalized);
+        else if constexpr (std::is_same<_Tp, signed short>::value) pushs(count, normalized);
+        else if constexpr (std::is_same<_Tp, unsigned short>::value) pushus(count, normalized);
+        else std::__throw_bad_function_call();
+    }
+
+    inline unsigned int getStride() const noexcept  { return stride; }
+
+    inline const std::vector<struct VertexBufferLayout::VertexArrayAttrib>& operator()() const noexcept
+    {
+        return layout;
     }
 
 };
--- source/pe_vertexbufferlayout.cpp
+++ source/pe_vertexbufferlayout.cpp
@@ -3,10 +3,78 @@
 
 namespace px {
 
-    unsigned int VertexBufferLayout::VertexArrayAttrib::getStride(unsigned int GLtype)
-    {
+    VertexBufferLayout::VertexBufferLayout(): stride(0) {}
 
-        return 0;
+    void VertexBufferLayout::pushf(unsigned int count, bool normalized)
+    {
+        this->insert(
+            GLtype<float>::enumval, count, sizeof(GLtype<float>::type),
+            (unsigned char)(normalized ? GL_TRUE : GL_FALSE)
+        );
+        this->stride += sizeof(GLtype<float>::type)*count;
+    }
+
+    void VertexBufferLayout::pushd(unsigned int count, bool normalized)
+    {
+        this->insert(
+            GLtype<double>::enumval, count, sizeof(GLtype<double>::type),
+            (unsigned char)(normalized ? GL_TRUE : GL_FALSE)
+        );
+        this->stride += sizeof(GLtype<double>::type)*count;
+    }
+
+    void VertexBufferLayout::pushb(unsigned int count, bool normalized)
+    {
+        this->insert(
+            GLtype<signed char>::enumval, count, sizeof(GLtype<signed char>::type),
+            (unsigned char)(normalized ? GL_TRUE : GL_FALSE)
+        );
+        this->stride += sizeof(GLtype<signed char>::type)*count;
+    }
+
+    void VertexBufferLayout::pushub(unsigned int count, bool normalized)
+    {
+        this->insert(
+            GLtype<unsigned char>::enumval, count, sizeof(GLtype<unsigned char>::type),
+            (unsigned char)(normalized ? GL_TRUE : GL_FALSE)
+        );
+        this->stride += sizeof(GLtype<unsigned char>::type)*count;
+    }
+
+    void VertexBufferLayout::pushi(unsigned int count, bool normalized)
+    {
+        this->insert(
+            GLtype<signed int>::enumval, count, sizeof(GLtype<signed int>::type),
+            (unsigned char)(normalized ? GL_TRUE : GL_FALSE)
+        );
+        this->stride += sizeof(GLtype<signed int>::type)*count;
+    }
+
+    void VertexBufferLayout::pushui(unsigned int count, bool normalized)
+    {
+        this->insert(
+            GLtype<unsigned int>::enumval, count, sizeof(GLtype<unsigned int>::type),
+            (unsigned char)(normalized ? GL_TRUE : GL_FALSE)
+        );
+        this->stride += sizeof(GLtype<unsigned int>::type)*count;
+    }
+
+    void VertexBufferLayout::pushs(unsigned int count, bool normalized)
+    {
+        this->insert(
+            GLtype<signed short>::enumval, count, sizeof(GLtype<signed short>::type),
+            (unsigned char)(normalized ? GL_TRUE : GL_FALSE)
+        );
+        this->stride += sizeof(GLtype<signed short>::type)*count;
+    }
+
+    void VertexBufferLayout::pushus(unsigned int count, bool normalized)
+    {
+        this->insert(
+            GLtype<unsigned short>::enumval, count, sizeof(GLtype<unsigned short>::type),
+            (unsigned char)(normalized ? GL_TRUE : GL_FALSE)
+        );
+        this->stride += sizeof(GLtype<unsigned short>::type)*count;
     }
 
 } // namespace px 
