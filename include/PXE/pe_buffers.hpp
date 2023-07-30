c9d6671da337efe3c1e213ff626e6149e4869586
PXblaZe <gsumit178@gmail.com>
2023-07-31 04:13:30 +0530

    Implemented VertexBuffer and ElementBuffer classes.

---
+++ include/PXE/pe_buffers.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+namespace px {
+
+class VertexBuffer {
+
+    unsigned int vboID;
+
+public:
+
+    const unsigned int bufferSize;
+
+    VertexBuffer(unsigned int size, const void* data = nullptr);
+    VertexBuffer(unsigned int size, unsigned int glUsage, const void* data = nullptr);
+    ~VertexBuffer();
+
+    void updateBufferData(const void* data, unsigned int offset = 0, size_t size = 0) const;
+
+    void getBufferData(void* data, unsigned int offset = 0, size_t size = 0) const;
+
+    void bind() const;
+
+    static void unbind();
+
+};
+
+class ElementBuffer {
+
+    unsigned int iboID;
+    unsigned int count;
+    unsigned int type_size;
+
+    void create(unsigned int type_size, unsigned int count, const void* data);
+
+    template<typename... Args>
+    static constexpr bool isIntegrals = (std::is_integral_v<Args> && ...);
+
+    template <typename First, typename... Next>
+    inline constexpr unsigned int sizeofFirstDataType(First first, Next... args) { return sizeof(First); }
+
+    template <typename First, typename... Next>
+    inline constexpr const First* firstPointer(First first, Next... args) const
+    { 
+        First* pData = (First*)malloc(sizeof(First)*((sizeof...(args))+1));
+        unsigned int i = 1; pData[0] = first;
+        ((pData[i++] = args), ...);
+        return pData;
+    }
+
+public:
+
+    template<typename _Tp>
+    ElementBuffer(unsigned int count, const _Tp* data) { create(sizeof(_Tp), count, data); }
+
+    template<typename... Args, typename = std::enable_if_t<isIntegrals<Args...>>>
+    ElementBuffer(Args... data)
+    {
+        create(sizeofFirstDataType(data...), sizeof...(Args), firstPointer(data...));
+    }
+
+    ~ElementBuffer();
+
+    void updateBufferData(const void* data, unsigned int offset = 0, size_t size = 0) const;
+
+    void getBufferData(void* data, unsigned int offset = 0, size_t size = 0) const;
+
+    const unsigned int draw_callType() const;
+
+    void bind() const;
+
+    static void unbind();
+
+};
+
+}
---
+++ source/pe_buffers.cpp
@@ -0,0 +1,85 @@
+
+#include <iostream>
+
+#include "PXE/pe_init.hpp"
+#include "PXE/pe_buffers.hpp"
+
+namespace px {
+
+    VertexBuffer::VertexBuffer(unsigned int size,const void* data): bufferSize(size)
+    {
+        PXcall(glGenBuffers(1, &vboID));
+        PXcall(glBindBuffer(GL_ARRAY_BUFFER, vboID));
+        PXcall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
+    }
+    
+    VertexBuffer::VertexBuffer(unsigned int size, unsigned int glUsage, const void* data): bufferSize(size)
+    {
+        PXcall(glGenBuffers(1, &vboID));
+        PXcall(glBindBuffer(GL_ARRAY_BUFFER, vboID));
+        PXcall(glBufferData(GL_ARRAY_BUFFER, size, data, glUsage));
+    }
+
+    VertexBuffer::~VertexBuffer() {
+        PXcall(glDeleteBuffers(1, &vboID));
+    }
+
+    void VertexBuffer::updateBufferData(const void *data, unsigned int offset, size_t size) const {
+        PXcall(glBufferSubData(GL_ARRAY_BUFFER, offset, size ? size : bufferSize-offset, data));
+    }
+
+    void VertexBuffer::getBufferData(void *data, unsigned int offset, size_t size) const {
+        PXcall(glGetBufferSubData(GL_ARRAY_BUFFER, offset, size ? size : bufferSize-offset, data));
+    }
+
+    void VertexBuffer::bind() const {
+        PXcall(glBindBuffer(GL_ARRAY_BUFFER, vboID));
+    }
+
+    void VertexBuffer::unbind() {
+        PXcall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    }
+
+
+    
+    ElementBuffer::~ElementBuffer() {
+        PXcall(glDeleteBuffers(1, &iboID));
+    }
+
+    void ElementBuffer::create(unsigned int type_size, unsigned int count, const void* data)
+    {
+        this->count = count;
+        this->type_size = type_size;
+        PXcall(glGenBuffers(1, &iboID));
+        PXcall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboID));
+        PXcall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, type_size*count, data, GL_STATIC_DRAW));
+    }
+
+    void ElementBuffer::updateBufferData(const void *data, unsigned int offset, size_t size) const {
+        PXcall(glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, offset, size ? size : type_size*count-offset, data));
+    }
+
+    void ElementBuffer::getBufferData(void *data, unsigned int offset, size_t size) const {
+        PXcall(glGetBufferSubData(GL_ELEMENT_ARRAY_BUFFER, offset, size ? size : type_size*count-offset, data));
+    }
+
+    const unsigned int ElementBuffer::draw_callType() const 
+    {
+        switch(type_size)
+        {
+            case sizeof(GLubyte): return GL_UNSIGNED_BYTE;
+            case sizeof(GLushort): return GL_UNSIGNED_SHORT;
+            case sizeof(GLuint): return GL_UNSIGNED_INT;
+        }
+        return 0;
+    }
+
+    void ElementBuffer::bind() const {
+        PXcall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboID));
+    }
+
+    void ElementBuffer::unbind() {
+        PXcall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+    }
+
+} // namespace px
