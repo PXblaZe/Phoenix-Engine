9c65919183ca724159fee32b5dd15d5c842eff0e
PXblaZe <gsumit178@gmail.com>
2023-08-14 22:05:28 +0530

    fix: active uniform is not found.

--- testShader.glsl
+++ testShader.glsl
@@ -3,11 +3,14 @@
 #version 450 
 
 layout(location=0) in vec3 pos;
+uniform float alpha;
+out float ch_a;
 
 out vec3 col;
 
 void main()
 {
+    ch_a = alpha;
     col = exp(pos-1);
     gl_Position = vec4(pos, 1);
 }
@@ -19,10 +22,13 @@ void main()
 
 out vec4 color;
 in vec3 col;
-uniform vec4 c;
+uniform vec3 c[3];
+in float ch_a;
 
 void main()
 {
-    color = vec4(col, 1);
-}
+    // color = vec4(abs(cross(col, c)), 1);
+    // color = vec4(c[0].g, c[1].r, c[2].r, 1);
+    color = vec4(pow(cross(col, vec3(c[0].b, c[1].b, c[2].b)), vec3(2)), ch_a);
 
+}
