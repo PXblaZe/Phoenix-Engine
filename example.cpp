0d7ecde1b75b1543a1340de4a9f33e1a6bc9846d
PXblaZe <gsumit178@gmail.com>
2023-08-14 22:54:18 +0530

    updated example.

--- example.cpp
+++ example.cpp
@@ -16,8 +16,8 @@ void rcb(const px::VertexArray& vao, const px::ElementBuffer& ibo) {
 
 int main() {
     
-    // px::Window win(800, 600, "Phoenix Engine");
-    px::Window win("While Noise");
+    px::Window win(800, 600, "Phoenix Engine");
+    // px::Window win("While Noise");
 
     GLfloat v[8] {
         -.5, .5,
@@ -31,21 +31,35 @@ int main() {
     px::VertexBufferLayout layout;
     layout.push<float>(2);
     vao.attachVertexBuffer(vbo, layout);
+    vbo.unbind();
 
     // GLbyte i[6] {0, 1, 2, 2, 3, 0};
-
     // px::ElementBuffer ibo(6, i);
+    
     px::ElementBuffer ibo((char)0, 1, 2, 2, 3, 0);
     std::cout << to_hex(ibo.draw_callType()) << std::endl;
 
     px::ShaderCode sc("testShader.glsl");
     px::ShaderProgram sp;
     sp.createProgram(sc.parseCode("ver"), sc.parseCode("frag"));
-    px::ShaderProgram::use(sp);
+
+    sp.idle();
+    sp.use();
+
+    for (px::ShaderProgram::UniAttriFormat ufm: sp.getUniformsList())
+        std::cout << "Uniform #" << ufm.index << " Type: 0x" << std::hex << ufm.type << ", Name: " << ufm.name << ", Size: " << ufm.arrayLength << ", Length: " << ufm.name.size() << std::endl;
+    
+
+    float colvals[9] = {0.f, 1.f, 0.f, 0.f, 0.f, 1.7f, 1.f, 0.f, 0.f};
+    sp.setUniform<3, 3>("c", colvals);
+
+    // sp.setUniform<3, 3>("c", 0.f, 1.f, 0.f, 0.f, 0.f, 1.f, 1.f, 0.f, 0.f);
+    sp.setUniform("alpha", 1.f);
+
 
     vao.unbind(), ibo.unbind();
 
-    // // std::cout << sc.parseCode("ver") << std::endl << sc.parseCode("frag") << std::endl;
+    // std::cout << sc.parseCode("ver") << std::endl << sc.parseCode("frag") << std::endl;
     // std::cout << "vsID: " << sp.getShaderID(px::ShaderProgram::VERTEX_SHADER) << std::endl;
     // std::cout << "fsID: " << sp.getShaderID(px::ShaderProgram::FRAGMENT_SHADER) << std::endl;
     sp.cleanShaders(px::ShaderProgram::FRAGMENT_SHADER | px::ShaderProgram::VERTEX_SHADER);
