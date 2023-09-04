cc6f533c7b52dca47ee3d8dfcb093405354d665e
PXblaZe <gsumit178@gmail.com>
2023-09-05 00:15:34 +0530

    Implemented basic Viewspace.

---
+++ include/PXE/pe_viewspace.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+
+namespace px
+{
+
+class Viewspace {
+    
+    glm::vec3 position, front, up, right, rotation, worldUp;
+
+    public:
+
+    Viewspace(const glm::vec3& position, const glm::vec3& rotationAngles = {0, 0, 0}, const glm::vec3& worldUp = {0, 1, 0});
+
+    void updateOrientation();
+
+    inline void setPosition(const glm::vec3& value) noexcept { position = value; }
+
+    inline void setFrontVector(const glm::vec3& value) noexcept { front = value; }
+
+    inline void setUpVector(const glm::vec3& value) noexcept { up = value; }
+
+    inline void setRightVector(const glm::vec3& value) noexcept { right = value; }
+
+    inline void setPitch(float radians) noexcept { rotation.x = radians; }
+
+    inline void setYaw(float radians) noexcept { rotation.y = radians; }
+
+    inline void setRoll(float radians) noexcept { rotation.z = radians; }
+
+    const glm::mat4& getViewMatrix() const;
+
+    inline const glm::vec3& getPosition() const noexcept { return position; }
+
+    inline const glm::vec3& getFrontVector() const noexcept { return front; }
+
+    inline const glm::vec3& getUpVector() const noexcept { return up; }
+
+    inline const glm::vec3& getRightVector() const noexcept { return right; }
+
+    inline const float& getPitch() const noexcept { return rotation.x; }
+
+    inline const float& getYaw() const noexcept { return rotation.y; } 
+
+    inline const float& getRoll() const noexcept { return rotation.z; }
+
+};
+
+} // namespace px
---
+++ source/pe_viewspace.cpp
@@ -0,0 +1,49 @@
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "PXE/pe_viewspace.hpp"
+
+namespace px
+{
+    
+    Viewspace::Viewspace(const glm::vec3& position, const glm::vec3& rotationAngles, const glm::vec3& worldUp)
+    : front(glm::vec3(0, 0, -1)) {
+        this->position = position;
+        this->worldUp = worldUp;
+        rotation = rotationAngles;
+        updateOrientation();
+    }
+
+    const glm::mat4& Viewspace::getViewMatrix() const
+    {
+        return glm::lookAt(position, position+front, up);
+    }
+
+    void Viewspace::updateOrientation()
+    {
+        float roll = rotation.z, pitch = rotation.x, yaw = rotation.y;
+
+        // Calculate new front vector
+        glm::vec3 newFront;
+        newFront.x = cos(yaw) * cos(pitch);
+        newFront.y = sin(pitch);
+        newFront.z = sin(yaw) * cos(pitch);
+        front = glm::normalize(newFront);
+
+        // Calculate new right and up vectors
+        right = glm::normalize(glm::cross(front, worldUp));
+        up = glm::normalize(glm::cross(right, front));
+
+        // Precalculate roll rotation matrix elements
+        float cosRoll = cos(roll);
+        float sinRoll = sin(roll);
+
+        // Apply roll rotation to up and right vectors
+        glm::vec3 newUp = up * cosRoll - right * sinRoll;
+        glm::vec3 newRight = up * sinRoll + right * cosRoll;
+
+        up = newUp;
+        right = newRight;
+    }
+
+} // namespace px
+ 
